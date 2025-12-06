9ccfae28d7ee50af212d4c1238154abe562b09b4
mohamed <you@example.com>
2025-12-06 18:33:48 +0000

    Fix ALLOWED_HOSTS env crash

--- ethsite/settings.py
+++ ethsite/settings.py
@@ -13,7 +13,7 @@ load_dotenv(os.path.join(BASE_DIR, ".env"))
 # ===============================
 SECRET_KEY = os.getenv("SECRET_KEY")
 DEBUG = os.getenv("DEBUG") == "True"
-ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS").split(",")
+ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS", "localhost,127.0.0.1,0.0.0.0").split(",")
 
 # ===============================
 # APPLICATION DEFINITION
